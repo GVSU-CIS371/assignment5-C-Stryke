55f4ebc6aac91dcf56eca5e05732c8cc10a6ca8b
C-Stryke <cjm03082020@gmail.com>
2025-12-04 18:48:34 +0000

    chaged Domain

--- src/firebase.ts
+++ src/firebase.ts
@@ -5,9 +5,9 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyD99666drVz_ZYI3wx75IAkSDb4Pn8-IwM",
-  authDomain: "cis-371-4eff3.firebaseapp.com",
-  projectId: "cis-371-4eff3",
-  storageBucket: "cis-371-4eff3.firebasestorage.app",
+  authDomain: "https://gvsu-cis371.github.io/assignment5-C-Stryke/.firebaseapp.com",
+  projectId: "https://gvsu-cis371.github.io/assignment5-C-Stryke/",
+  storageBucket: "https://gvsu-cis371.github.io/assignment5-C-Stryke/.firebasestorage.app",
   messagingSenderId: "772947264565",
   appId: "1:772947264565:web:214eb352e7f99894da1d7c",
 };
